cede96cb4fa1a1dd77fba7585d0cf56ad30d4bed
John-McPherson <john-mac1989@hotmail.co.uk>
2021-10-20 15:26:51 +0000

    Add js for playing sound and animation

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function playSound(e){
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('playing')
+}
+
+let keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e){
+    if(e.propertyName !=='transform') return;
+    this.classList.remove('playing')
+}
+window.addEventListener('keydown',playSound);
